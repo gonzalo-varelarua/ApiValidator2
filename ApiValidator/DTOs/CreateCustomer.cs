6a496854913532119991adfded2a4956f139d0ee
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-11-10 10:12:51 -0300

    IValidatableObject

--- ApiValidator/DTOs/CreateCustomer.cs
+++ ApiValidator/DTOs/CreateCustomer.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using ApiValidator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ApiValidator.DTOs
 {
-    public class CreateCustomer
+    public class CreateCustomer : IValidatableObject
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime DateofBirth { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+
+            if (!ValidateEmail.IsValidEmail(Email))
+                errors.Add(new ValidationResult("Email inválido.", new[] { nameof(Email) }));
+
+            if (string.IsNullOrEmpty(Name))
+                errors.Add(new ValidationResult("El nombre no puede ser nulo.", new[] { nameof(Name) }));
+
+            var diffDays = (DateTime.Now - DateofBirth).TotalDays;
+            var age = Math.Truncate(diffDays / 365);
+            if (age <= 18)
+            {
+                errors.Add(new ValidationResult("Es menor de edad.", new[] { nameof(DateofBirth) }));
+            }
+
+            return errors;
+        }
     }
 }
