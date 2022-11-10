0541501872fe0883e11087707f63caa3502ed7ff
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-11-10 09:55:56 -0300

    Endpoint de Prueba

---
+++ ApiValidator/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using ApiValidator.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApiValidator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateCustomer create)
+        {
+
+            return Created("uri", create);
+        }
+
+    }
+}
---
+++ ApiValidator/DTOs/CreateCustomer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ApiValidator.DTOs
+{
+    public class CreateCustomer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateofBirth { get; set; }
+    }
+}
